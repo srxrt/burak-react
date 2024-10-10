b346f98c1ff10d1b8443e418620150a1397e32fe
srxrt <str9703@gmail.com>
2024-10-10 15:23:36 +0900

    fix: organize homepage related sectional components

---
+++ src/app/screens/homePage/Events.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export function Events() {
+	return <div>Events</div>;
+}
---
+++ src/app/screens/homePage/NewDishes.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+export function NewDishes() {
+	return (
+		<div>
+			<Container>New Dishes</Container>
+		</div>
+	);
+}
