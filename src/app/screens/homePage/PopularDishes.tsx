cd09f712ecc94bedbd7547549c7db62bc167ab77
srxrt <str9703@gmail.com>
2024-10-11 11:52:34 +0900

    fix: modify popular and new dishes components

--- src/app/screens/homePage/PopularDishes.tsx
+++ src/app/screens/homePage/PopularDishes.tsx
@@ -23,63 +23,67 @@ export default function PopularDishes() {
 				<Stack className="popular-section">
 					<Box className="category-title">Popular Dishes</Box>
 					<Stack className="cards-frame">
-						{list.map((ele, index) => {
-							return (
-								<CssVarsProvider key={index}>
-									<Card className={"card"}>
-										<CardCover>
-											<img src={ele.imagePath} alt="" />
-										</CardCover>
-										<CardCover className="card-cover" />
-										<CardContent sx={{ justifyContent: "flex-end" }}>
-											<Stack
-												flexDirection={"row"}
-												justifyContent={"space-between"}
+						{list.length !== 0 ? (
+							list.map((ele, index) => {
+								return (
+									<CssVarsProvider key={index}>
+										<Card className={"card"}>
+											<CardCover>
+												<img src={ele.imagePath} alt="" />
+											</CardCover>
+											<CardCover className="card-cover" />
+											<CardContent sx={{ justifyContent: "flex-end" }}>
+												<Stack
+													flexDirection={"row"}
+													justifyContent={"space-between"}
+												>
+													<Typography
+														level="h2"
+														fontSize="lg"
+														textColor="#fff"
+														mb={1}
+													>
+														{ele.productName}
+													</Typography>
+													<Typography
+														sx={{
+															fontWeight: "md",
+															color: "neutral.300",
+															alignItems: "center",
+															display: "flex",
+														}}
+													>
+														20
+														<Visibility
+															sx={{ fontSize: 25, marginLeft: "5px" }}
+														/>
+													</Typography>
+												</Stack>
+											</CardContent>
+											<CardOverflow
+												sx={{
+													display: "flex",
+													gap: 1.5,
+													py: 1.5,
+													px: "var(--Card-padding)",
+													borderTop: "1px solid",
+													height: "60px",
+												}}
 											>
 												<Typography
-													level="h2"
-													fontSize="lg"
-													textColor="#fff"
-													mb={1}
+													startDecorator={<DescriptionOutlined />}
+													text-color="neutral.300"
 												>
-													{ele.productName}
+													This is delicious meal
 												</Typography>
-												<Typography
-													sx={{
-														fontWeight: "md",
-														color: "neutral.300",
-														alignItems: "center",
-														display: "flex",
-													}}
-												>
-													20
-													<Visibility
-														sx={{ fontSize: 25, marginLeft: "5px" }}
-													/>
-												</Typography>
-											</Stack>
-										</CardContent>
-										<CardOverflow
-											sx={{
-												display: "flex",
-												gap: 1.5,
-												py: 1.5,
-												px: "var(--Card-padding)",
-												borderTop: "1px solid",
-												height: "60px",
-											}}
-										>
-											<Typography
-												startDecorator={<DescriptionOutlined />}
-												text-color="neutral.300"
-											>
-												This is delicious meal
-											</Typography>
-										</CardOverflow>
-									</Card>
-								</CssVarsProvider>
-							);
-						})}
+											</CardOverflow>
+										</Card>
+									</CssVarsProvider>
+								);
+							})
+						) : (
+							<Box className="no-data">Popular products are not available!</Box>
+						)}
 					</Stack>
 				</Stack>
 			</Container>
