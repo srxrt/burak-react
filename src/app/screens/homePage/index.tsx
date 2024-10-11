4e58cfcb745dbfb280225572ff19aa5ff370f5d2
srxrt <str9703@gmail.com>
2024-10-11 17:18:54 +0900

    fix: fixed events sectional component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -5,7 +5,7 @@ import PopularDishes from "./PopularDishes";
 import { NewDishes } from "./NewDishes";
 import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
-import { Events } from "./Events";
+import Events from "./Events";
 import "../../../css/home.css";
 export default function HomePage() {
 	return (
