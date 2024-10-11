cf991617b64ef029b89ef42f93f7fbd868bbbf22
srxrt <str9703@gmail.com>
2024-10-11 10:54:51 +0900

    feat: develop popular dishes sectional component

--- src/app/screens/homePage/PopularDishes.tsx
+++ src/app/screens/homePage/PopularDishes.tsx
@@ -1,10 +1,88 @@
 import React from "react";
-import { Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
+import {
+	CardCover,
+	Card,
+	CardContent,
+	CardOverflow,
+	CssVarsProvider,
+	Typography,
+} from "@mui/joy";
+import { DescriptionOutlined, Visibility } from "@mui/icons-material";
 
+const list = [
+	{ productName: "Lavash", imagePath: "/img/lavash.webp" },
+	{ productName: "Cutlet", imagePath: "/img/cutlet.webp" },
+	{ productName: "Kebab", imagePath: "/img/kebab.webp" },
+	{ productName: "Kebab", imagePath: "/img/kebab-fresh.webp" },
+];
 export default function PopularDishes() {
 	return (
-		<div>
-			<Container>Popular Dishes</Container>
+		<div className="popular-dishes-frame">
+			<Container>
+				<Stack className="popular-section">
+					<Box className="category-title">Popular Dishes</Box>
+					<Stack className="cards-frame">
+						{list.map((ele, index) => {
+							return (
+								<CssVarsProvider key={index}>
+									<Card className={"card"}>
+										<CardCover>
+											<img src={ele.imagePath} alt="" />
+										</CardCover>
+										<CardCover className="card-cover" />
+										<CardContent sx={{ justifyContent: "flex-end" }}>
+											<Stack
+												flexDirection={"row"}
+												justifyContent={"space-between"}
+											>
+												<Typography
+													level="h2"
+													fontSize="lg"
+													textColor="#fff"
+													mb={1}
+												>
+													{ele.productName}
+												</Typography>
+												<Typography
+													sx={{
+														fontWeight: "md",
+														color: "neutral.300",
+														alignItems: "center",
+														display: "flex",
+													}}
+												>
+													20
+													<Visibility
+														sx={{ fontSize: 25, marginLeft: "5px" }}
+													/>
+												</Typography>
+											</Stack>
+										</CardContent>
+										<CardOverflow
+											sx={{
+												display: "flex",
+												gap: 1.5,
+												py: 1.5,
+												px: "var(--Card-padding)",
+												borderTop: "1px solid",
+												height: "60px",
+											}}
+										>
+											<Typography
+												startDecorator={<DescriptionOutlined />}
+												text-color="neutral.300"
+											>
+												This is delicious meal
+											</Typography>
+										</CardOverflow>
+									</Card>
+								</CssVarsProvider>
+							);
+						})}
+					</Stack>
+				</Stack>
+			</Container>
 		</div>
 	);
 }
