b8cfdecaf6e834c10bbe6341dc27b3e294f32839
srxrt <str9703@gmail.com>
2024-10-11 11:37:21 +0900

    feat: develop newDishes sectional component

--- src/app/screens/homePage/NewDishes.tsx
+++ src/app/screens/homePage/NewDishes.tsx
@@ -1,10 +1,64 @@
 import React from "react";
-import { Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
+import {
+	AspectRatio,
+	Card,
+	CardOverflow,
+	CssVarsProvider,
+	Typography,
+} from "@mui/joy";
+import Divider from "../../components/divider";
+import { Visibility } from "@mui/icons-material";
 
+const newDishes = [
+	{ productName: "Lavash", imagePath: "/img/lavash.webp" },
+	{ productName: "Cutlet", imagePath: "/img/cutlet.webp" },
+	{ productName: "Kebab", imagePath: "/img/kebab.webp" },
+	{ productName: "Kebab", imagePath: "/img/kebab-fresh.webp" },
+];
 export function NewDishes() {
 	return (
-		<div>
-			<Container>New Dishes</Container>
+		<div className="new-products-frame">
+			<Container>
+				<Stack className="main">
+					<Box className="category-title">Fresh Menu</Box>
+					<Stack className="cards-frame">
+						<CssVarsProvider>
+							{newDishes.map((ele, index) => {
+								return (
+									<Card key={index} variant="outlined" className="card">
+										<CardOverflow>
+											<div className="product-sale">Normal Size</div>
+											<AspectRatio ratio="1">
+												<img src={ele.imagePath} alt="" />
+											</AspectRatio>
+										</CardOverflow>
+										<CardOverflow variant="soft" className="product-detail">
+											<Stack className="info">
+												<Stack flexDirection={"row"}>
+													<Typography className="title">
+														{ele.productName}
+													</Typography>
+													<Divider width="2" height="24" bg="#d9d9d9" />
+													<Typography className="price">$12</Typography>
+												</Stack>
+												<Stack>
+													<Typography className="views">
+														20
+														<Visibility
+															sx={{ fontSize: 20, marginLeft: "5px" }}
+														/>
+													</Typography>
+												</Stack>
+											</Stack>
+										</CardOverflow>
+									</Card>
+								);
+							})}
+						</CssVarsProvider>
+					</Stack>
+				</Stack>
+			</Container>
 		</div>
 	);
 }
